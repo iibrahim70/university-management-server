7057efb3a16deeaf0e5ce1bf7627fc0ba033c4a1
iibrahim70 <aviknuhash70@gmail.com>
2023-11-30 21:13:27 +0600

    added prettier and eslint setup

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
-import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 const app = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Dev!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Dev!');
 });
 
-app.all("*", (req: Request, res: Response) => {
+app.all('*', (req: Request, res: Response) => {
   res.status(404).json({
     success: false,
-    message: "Route Not Found",
+    message: 'Route Not Found',
   });
 });
 
 // global error handler
-app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-  if (error) {
-    res.status(400).json({
-      success: false,
-      message: "Something went wrong",
-    });
-  }
-});
+// app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+//   if (error) {
+//     res.status(400).json({
+//       success: false,
+//       message: "Something went wrong",
+//     });
+//   }
+// });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
-import mongoose from "mongoose";
-import { Server } from "http";
-import app from "./app";
-import config from "./app/config";
+import mongoose from 'mongoose';
+import { Server } from 'http';
+import app from './app';
+import config from './app/config';
 
 let server: Server;
 
 async function main() {
   try {
     const connectionInstance = await mongoose.connect(
-      `${config.mongoURI}/${config.dbName}`
+      `${config.mongoURI}/${config.dbName}`,
     );
     console.log(
-      `MongoDB Connected! DB Host: ${connectionInstance.connection.host}`
+      `MongoDB Connected! DB Host: ${connectionInstance.connection.host}`,
     );
     server = app.listen(config.port, () => {
       console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
-    console.log("MongoDB Error:", error);
+    console.log('MongoDB Error:', error);
     process.exit(1);
   }
 }
