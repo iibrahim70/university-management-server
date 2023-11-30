c2a079e2bcc7d4c43cae169145769f006dcc515c
iibrahim70 <aviknuhash70@gmail.com>
2023-11-30 15:59:47 +0600

    added error handler middleware

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// parsers
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello Dev!");
+});
+app.all("*", (req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route Not Found",
+    });
+});
+// global error handler
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong",
+        });
+    }
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
+
+// parsers
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Dev!");
+});
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
 });
 
 export default app;
