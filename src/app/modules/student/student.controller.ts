3c889ca9bcd110bf7e493fc9519e6bc1e17dde95
iibrahim70 <aviknuhash70@gmail.com>
2023-12-24 20:03:22 +0600

    added catchasync higher order function

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,38 +1,30 @@
-import { RequestHandler } from 'express';
-import { StudentServices } from './student.service';
-import { sendResponse } from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import { StudentServices } from './student.service';
+import { catchAsync } from '../../utils/catchAsync';
+import { sendResponse } from '../../utils/sendResponse';
 
-const getAllStudents: RequestHandler = async (req, res, next) => {
-  try {
-    const result = await StudentServices.getAllStudentsFromDB();
+const getAllStudents = catchAsync(async (req, res) => {
+  const result = await StudentServices.getAllStudentsFromDB();
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Students are retrieved successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students are retrieved successfully',
+    data: result,
+  });
+});
 
-const getsSingleStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { studentId } = req.params;
-    const result = await StudentServices.getSingleStudentFromDB(studentId);
+const getsSingleStudent = catchAsync(async (req, res) => {
+  const { studentId } = req.params;
+  const result = await StudentServices.getSingleStudentFromDB(studentId);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Students is retrieved successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students is retrieved successfully',
+    data: result,
+  });
+});
 
 export const StudentControllers = {
   getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,27 +1,20 @@
-import { RequestHandler } from 'express';
-import { UserServices } from './user.service';
-import { sendResponse } from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import { UserServices } from './user.service';
+import { catchAsync } from '../../utils/catchAsync';
+import { sendResponse } from '../../utils/sendResponse';
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { password, studentData } = req.body;
+const createStudent = catchAsync(async (req, res) => {
+  const { password, studentData } = req.body;
 
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
+  const result = await UserServices.createStudentIntoDB(password, studentData);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
